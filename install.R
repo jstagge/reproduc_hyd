d0762fc834b5e7c8cf648c091ca8e1cec75f83ee
Adel M. Abdallah <amabdallah@aggiemail.usu.edu>
2018-10-16 19:45:53 -0600

    Installs R packages on MyBinder

---
+++ install.R
@@ -0,0 +1,36 @@
+# *------------------------------------------------------------------
+# | PROGRAM NAME: 00_prepare_file_system
+# | FILE NAME: 00_prepare_file_system.R
+# | DATE: 
+# | CREATED BY:  Jim Stagge         
+# *----------------------------------------------------------------
+# | PURPOSE:  This code installs all packages needed to run the 
+# | reproducibility analysis.
+# | 
+# | 
+# |
+# *------------------------------------------------------------------
+
+###########################################################################
+###  Install packages 
+###########################################################################
+### Packages from CRAN
+install.packages("assertthat")
+install.packages("tidyverse")
+install.packages("colorblindr")
+
+install.packages("stringr")
+install.packages("ggridges")
+install.packages("ggthemes")
+install.packages("riverplot")
+install.packages("tspmeta")
+install.packages("svglite")
+install.packages("MultinomialCI")
+
+### Packages from GitHub
+install.packages("devtools")
+require(devtools)
+
+install_github("jalvesaq/colorout")
+install_github("jstagge/staggefuncs")
+
